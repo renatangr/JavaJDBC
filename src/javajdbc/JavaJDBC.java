040e8c0d7a6c51b0a8776598192f11a7b994ed03
Renata Negrini | Estudante de ADS | 4º Sem <renatanegrinim@gmail.com>
2023-10-25 17:10:42 -0300

    Update Project

--- src/javajdbc/JavaJDBC.java
+++ src/javajdbc/JavaJDBC.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package javajdbc;
 
 /**
@@ -10,11 +6,8 @@ package javajdbc;
  */
 public class JavaJDBC {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
